ef519d14de0ea848c2a3176db464777da823ed0d
Gavrilescu Anca <ankagavrilescu@gmail.com>
2017-08-03 18:15:48 +0300

    replace javascript code with scalatags :)

--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,21 +1,28 @@
 package example
 import scala.scalajs.js.annotation.JSExport
-import org.scalajs.dom
 import org.scalajs.dom.{CanvasRenderingContext2D, html}
+import org.scalajs.dom
+
+import scalatags.JsDom.all._
+import org.scalajs.dom.html.Div
+import sun.awt.geom.Crossings
 
 @JSExport
-object ScalaJSExample {
+object ScalaJSExample{
 
   def toRadians(degrees: Double) ={
     (degrees * math.Pi)/180
   }
 
+  @JSExport
   def drawLines(crossings: Int, canvas: html.Canvas) = {
     val context = canvas.getContext("2d")
       .asInstanceOf[dom.CanvasRenderingContext2D]
 
-    val centerX = Math.floor(canvas.width / 2) 
+    val centerX = Math.floor(canvas.width / 2)
+    println(centerX)
     val centerY = Math.floor(canvas.height / 2)
+    println(centerY)
     val radius = Math.floor(canvas.width / 2)
 
     val begin = 0
@@ -25,18 +32,23 @@ object ScalaJSExample {
     context.strokeStyle = "black"
     context.lineWidth = 2
 
-    drawArc(context, centerX, centerY, radius, 2*Math.PI, 0)
+    drawArc(context, centerX, centerY , radius, 2*Math.PI, 0)
 
     (0 to crossings*2-1).map(_*arcSize).foreach{startingAngle =>
-     // drawWithArrowhead(centerX+radius*Math.cos(startingAngle),centerY-radius*Math.sin(startingAngle), centerX, centerY, canvas);
+
     }
 
-    drawWithArrowhead(centerX+radius*Math.cos(2*arcSize),centerY-radius*Math.sin(2*arcSize), centerX+radius*Math.cos(7*arcSize),centerY-radius*Math.sin(7*arcSize), canvas);
-
+    pairings(crossings).toSeq.foreach{pair1 =>
+      val pair = pair1._1
+      var index = 0
+        pair.toSeq.foreach{p =>
+          drawWithArrowhead(centerX+radius*Math.cos(p._2*arcSize),centerY-radius*Math.sin(p._2*arcSize), centerX+radius*Math.cos(p._1*arcSize),centerY-radius*Math.sin(p._1*arcSize), canvas);
+        }
+    }
   }
 
   private def drawArc(context: CanvasRenderingContext2D, centerX: Double, centerY: Double, radius: Double, arcSize: Double, startingAngle: Double) = {
-    context.arc(centerX, centerY, radius, startingAngle, startingAngle + arcSize, false)
+    context.arc(centerX, centerY, radius, startingAngle, startingAngle + arcSize, true)
     context.stroke
   }
 
@@ -78,44 +90,28 @@ object ScalaJSExample {
 
   }
 
-  def calcAngle(x1: Double, y1:Double, x2: Double, y2: Double): Double = {
-    if(x2 > x1){
-      if(y2 > y1){
-        Math.atan((y2 - y1) / (x2 - x1))
-      }else{
-        if(y2 == y1){
-          0
-        }else{
-          2 * math.Pi + Math.atan((y2 - y1) / (x2 - x1))
-        }
-      }
-    }else{
-      if(x2 == x1){
-        if(y2 == y1){
-          0
-        }else{
-          if(y2 > y1){
-            math.Pi / 2
-          }else{
-            1.5 * math.Pi
-          }
-        }
-      }else{
-        if(y2 == y1){
-          math.Pi * 1
-        }else{
-          if(y2 > y1){
-            math.Pi + Math.atan((y2 - y1) / (x2 - x1))
-          }else{
-            math.Pi + Math.atan((y2 - y1) / (x2 - x1))
-          }
-        }
-      }
+  @JSExport
+  def pairings(n:Int) = {
+    K.configurations(n)
+  }
+
+  def pair(n: Int) = {
+    pairings(n).toSeq.foreach{pair =>
+      val set = pair._1
     }
   }
 
+  def drawCanvas(target: html.Div, idNo: Int): Unit = {
+    target.appendChild(
+        div(id := "div"+idNo, width := 400, height := 400, margin := 20,
+          canvas(id := "canvas"+idNo, attr("width") := 300, attr("height") := 300, attr("margin") := 50)
+        ).render
+      )
+  }
+
   @JSExport
-  def main(canvas: html.Canvas): Unit = {
-    drawLines(4,canvas)
+  def main(target: html.Div): Unit = {
+    for(it <- 1 to 13) yield
+      drawCanvas(target,it)
   }
 }
